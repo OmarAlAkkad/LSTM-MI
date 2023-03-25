ad37bc3b2c1dc8e228b1e81b136e6c259ea96fa2
OmarAlAkkad <omarakkad97@gmail.com>
2023-03-24 23:33:07 -0500

    split cifar dataset

--- CIFAR/cifarTargetShadowPreprocess.py
+++ CIFAR/cifarTargetShadowPreprocess.py
@@ -16,14 +16,15 @@ from sklearn.model_selection import train_test_split
 def create_dataset():
     #load mnist dataset from keras.
     train, test = tf.keras.datasets.cifar10.load_data()
-
     train_inputs = train[0]
     train_labels = train[1]
+    target_x_train, x_test, target_y_train, y_test = train_test_split(train_inputs, train_labels, test_size = 0.8, stratify=train_labels)
+    shadow_x_train, x_test, shadow_y_train, y_test = train_test_split(x_test, y_test, test_size = 0.75, stratify=y_test)
 
     test_inputs = test[0]
     test_labels = test[1]
 
-    return train_inputs, train_labels, test_inputs, test_labels
+    return target_x_train, target_y_train, shadow_x_train, shadow_y_train, test_inputs, test_labels
 
 def split_target_shadow(X,y,split):
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = split, stratify=y)
@@ -31,20 +32,16 @@ def split_target_shadow(X,y,split):
     return x_train, x_test, y_train, y_test
 
 if __name__ == "__main__":
-    train_inputs, train_labels, test_inputs, test_labels = create_dataset()
+    target_x_train, target_y_train, shadow_x_train, shadow_y_train, test_inputs, test_labels = create_dataset()
 
-    split = 0.5
-    target_train_inputs, shadow_train_inputs, target_train_labels, shadow_train_labels = split_target_shadow(train_inputs,train_labels,split)
-    target_test_inputs, shadow_test_inputs, target_test_labels, shadow_test_labels = split_target_shadow(test_inputs,test_labels,split)
-
-    pickle.dump(target_train_inputs, open('target_train_inputs.p', 'wb'))
-    pickle.dump(shadow_train_inputs, open('shadow_train_inputs.p', 'wb'))
-    pickle.dump(target_train_labels, open('target_train_labels.p', 'wb'))
-    pickle.dump(shadow_train_labels, open('shadow_train_labels.p', 'wb'))
-    pickle.dump(target_test_inputs, open('target_test_inputs.p', 'wb'))
-    pickle.dump(shadow_test_inputs, open('shadow_test_inputs.p', 'wb'))
-    pickle.dump(target_test_labels, open('target_test_labels.p', 'wb'))
-    pickle.dump(shadow_test_labels, open('shadow_test_labels.p', 'wb'))
+    pickle.dump(target_x_train, open('target_train_inputs.p', 'wb'))
+    pickle.dump(shadow_x_train, open('shadow_train_inputs.p', 'wb'))
+    pickle.dump(target_y_train, open('target_train_labels.p', 'wb'))
+    pickle.dump(shadow_y_train, open('shadow_train_labels.p', 'wb'))
+    pickle.dump(test_inputs, open('target_test_inputs.p', 'wb'))
+    pickle.dump(test_inputs, open('shadow_test_inputs.p', 'wb'))
+    pickle.dump(test_labels, open('target_test_labels.p', 'wb'))
+    pickle.dump(test_labels, open('shadow_test_labels.p', 'wb'))
 
 
 
